9a8b18247594bc7b9881807d57a7e4909be1fffa
Emery K Lumsden II <emerylumsden@gmail.com>
2020-04-20 19:49:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,23 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  phrase = "It's-a me, Mario!"
+  puts return phrase
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+describe "all_phrases" do 
+  it do
+  expect {all_phrases}.to output (mario).to_stdout
+  expect {all_phrases}.to output (toadstool).to_stdout
+  expect {all_phrases}.to output(link).to_stdout
+end
+end
+  
